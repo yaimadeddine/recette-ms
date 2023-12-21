424cdee52519cbc529e9d6342227366cb6d8708e
yaimadeddine <yo.aimadeddine@gmail.com>
2023-12-21 17:16:59 +0100

    adding eureka and gateway

--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -2,7 +2,10 @@ package com.example.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserApplication {
 
     public static void main(String[] args) {
