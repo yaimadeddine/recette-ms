9fd776dce38f03b4b29901db4a42b21d5eab2994
soulohabdellah <soulouhabdellah@gmail.com>
2023-12-24 13:51:54 +0100

    user test commit

--- user/src/test/java/com/example/user/controller/UserWsTest.java
+++ user/src/test/java/com/example/user/controller/UserWsTest.java
@@ -1,4 +1,52 @@
 package com.example.user.controller;
 
+import com.example.user.bean.User;
+import com.example.user.dao.UserDao;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserWsTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @Autowired
+    private UserDao userDao;
+
+    @BeforeEach
+    public void setUp() {
+        User testUser = new User(1, "XS12SLH0", "SOULOH", "Abdellah", "souloh@gmail.com", "1234");
+        userDao.save(testUser);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        userDao.deleteAll();
+    }
+
+    @Test
+    public void testFindByRef() {
+        ResponseEntity<User> response = restTemplate.getForEntity("/api/user/ref/{ref}", User.class, "XS12SLH0");
+        assertEquals(200, response.getStatusCodeValue());
+        assertNotNull(response.getBody());
+        assertEquals("SOULOH", response.getBody().getNom());
+        assertEquals("Abdellah", response.getBody().getPrenom());
+        assertEquals("souloh@gmail.com", response.getBody().getEmail());
+    }
+
+    @Test
+    public void testDeleteByRef() {
+        ResponseEntity<Integer> response = restTemplate.exchange("/api/user/ref/{ref}", HttpMethod.DELETE, null, Integer.class, "XS12SLH0");
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(1, response.getBody());
+    }
+
 }
